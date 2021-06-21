90be45bce80e8c8df48410e99415b9df36c4f55d
cw-daisuke.kasuya <cw-daisuke.kasuya@cw-daisukekasuyanoMacBook-Pro.local>
2021-06-21 17:53:48 +0900

    WordCountを解いた

--- src/main/scala/com/chatwork/quiz/misc/WordCounter.scala
+++ src/main/scala/com/chatwork/quiz/misc/WordCounter.scala
@@ -11,6 +11,8 @@ object WordCounter {
     * @param words 文字列
     * @return 単語がキー、単語数がヴァリューのマップ
     */
-  def countWords(words: List[String]): Map[String, Int] = ???
+  def countWords(words: List[String]): Map[String, Int] = {
+    words.flatMap(_.split(" ")).groupBy(s => s).map(xs => xs._1 -> xs._2.size)
+  }
 
 }
